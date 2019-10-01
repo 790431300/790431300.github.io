15a59b9f5d21e68bd72537e95a259f75e69511ce
790431300 <790431300@qq.com>
2019-10-01 12:28:15 +0800

    无

--- games/video/js.js
+++ games/video/js.js
@@ -13,7 +13,7 @@ if(path!=""){
 $("video").src=path;
 }
 //选台操作
-tv=new Array("https://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8",
+tv=new Array("http://yztv.wshls.homecdn.com/live/4387.m3u8",
 "http://cctvalih5c.v.myalicdn.com/live/cctv6_2/index.m3u8",
 "http://live-lt-hls-yf.jstv.com/live/cctv10_1000/online.m3u8",
 "https://w1live.livecdn.yicai.com/live/cbn.m3u8","http://mgzb.live.miguvideo.com:8088/wd-hunanhd-350/index.m3u8?msisdn=3FED9BE7AC2EBDE71F1236ADF0AA46AF&mdspid=&spid=699017&netType=0&sid=2201064496&pid=2028597799&timestamp=20190717231119&Channel_ID=H5_&ProgramID=619858170&ParentNodeID=-99&assertID=2201064496&client_ip=121.41.95.185&SecurityKey=20190717231119&promotionId=&mvid=&mcid=&mpid=&encrypt=101546f203cb499b59382d522ada86dc","http://cctvalih5c.v.myalicdn.com/live/cctv2_2/index.m3u8","http://121.31.30.90:8085/ysten-business/live/jinyingkaton/1.m3u8");
